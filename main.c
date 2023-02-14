635bccc201c3445feae9a429fb4d69740718c3a4
Zouhair Laaroussi <zouhairlrs@gmail.com>
2023-02-14 19:16:34 +0100

    commit main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 12:03:53 by zlaarous          #+#    #+#             */
-/*   Updated: 2023/02/14 15:31:30 by zlaarous         ###   ########.fr       */
+/*   Updated: 2023/02/14 19:16:10 by zlaarous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,11 @@
 int main(int argc, char **argv)
 {
 	t_element *stack_a;
-	// t_element *stack_b;
+	t_element *stack_b;
 	
 	if(argc == 1)
 		return (0);
-	// stack_a = malloc(sizeof(t_element));
-	// stack_b = malloc(sizeof(t_element));
-	
-	// stack_a->value = ft_atoi(argv[1]);
-	// stack_b->value = ft_atoi(argv[2]);
+		
 	parsing(&stack_a, argv, argc);
 	
-	
-	// printf("%d\n", stack_a->index);
-	// printf("%d\n", stack_b->index);
 }
