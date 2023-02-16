a68e3f2e883067117dbc383f011661e40c472cfd
Zouhair Laaroussi <92701395+ZoouhairLrs@users.noreply.github.com>
2023-02-16 15:03:30 +0100

    commit moving

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 12:03:53 by zlaarous          #+#    #+#             */
-/*   Updated: 2023/02/16 14:57:43 by zlaarous         ###   ########.fr       */
+/*   Updated: 2023/02/16 15:03:12 by zlaarous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ int main(int argc, char **argv)
 	t_element *stack_a;
 	t_element *stack_b;
 	
+	stack_b = NULL;
 	if(argc == 1)
 		return (0);
 		
 	parsing(&stack_a, argv, argc);
 	print_stacks(stack_a, stack_b);
-	
+	sort("ra", stack_a, stack_b);
+	print_stacks(stack_a, stack_b);
 	// printf("%d\n", stack_a->index);
 	// printf("%d\n", stack_b->index);
 }
