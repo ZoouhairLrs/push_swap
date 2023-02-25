bc2ccc103126c10781963128662d640d230c6d35
Zouhair Laaroussi <zouhairlrs@gmail.com>
2023-02-25 01:15:20 +0100

    commit

--- move1.c
+++ move1.c
@@ -6,7 +6,7 @@
 /*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 15:29:08 by zlaarous          #+#    #+#             */
-/*   Updated: 2023/02/16 14:56:52 by zlaarous         ###   ########.fr       */
+/*   Updated: 2023/02/19 18:36:10 by zlaarous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,16 @@
 
 void	sort(char *move, t_element *stack_a, t_element *stack_b)
 {
-	(void)stack_a;
-	(void)stack_b;
+	// (void)stack_a;
+	// (void)stack_b;
 
 	if(ft_strcmp(move, "ra"))
-		printf("\nra");
+	{
+		if(stack_a)
+		{
+			stack_b->value = stack_a->value;
+		}
+	}
 	else if(ft_strcmp(move, "rb"))
 		printf("\nrb");
 }
