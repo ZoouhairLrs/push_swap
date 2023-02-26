58138bbdc23e0b4fad4f62fae11cb80774aff922
Zouhair Laaroussi <zouhairlrs@gmail.com>
2023-02-26 17:45:39 +0100

    commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 12:03:53 by zlaarous          #+#    #+#             */
-/*   Updated: 2023/02/25 19:22:58 by zlaarous         ###   ########.fr       */
+/*   Updated: 2023/02/26 17:43:38 by zlaarous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,28 @@ int main(int argc, char **argv)
 {
 	t_element *stack_a;
 	t_element *stack_b;
-	
+
 	stack_b = NULL;
 	stack_a = NULL;
 	if(argc == 1)
 		return (0);
-		
+
 	parsing(&stack_a, argv, argc);
 	print_stacks(stack_a, stack_b);
-	
+
 	printf("\n************\n\n");
 	sort("pb", &stack_a, &stack_b);
 	print_stacks(stack_a, stack_b);
-	
+
 	printf("\n************\n\n");
 	sort("pa", &stack_a, &stack_b);
 	print_stacks(stack_a, stack_b);
-	
+
 	printf("\n************\n\n");
 	sort("ra", &stack_a, &stack_b);
 	print_stacks(stack_a, stack_b);
-	
-	printf("\n************\n\n");
-	sort("rb", &stack_a, &stack_b);
-	print_stacks(stack_a, stack_b);
+
+	// printf("\n************\n\n");
+	// sort("rb", &stack_a, &stack_b);
+	// print_stacks(stack_a, stack_b);
 }
