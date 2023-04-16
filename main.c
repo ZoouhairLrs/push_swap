2c5f2fdfc8aff2c39af8dc669c053df7f4afbfeb
Zouhair Laaroussi <zouhairlrs@gmail.com>
2023-04-16 21:14:00 +0000

    commit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 12:03:53 by zlaarous          #+#    #+#             */
-/*   Updated: 2023/02/26 17:43:38 by zlaarous         ###   ########.fr       */
+/*   Updated: 2023/04/16 21:13:13 by zlaarous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,39 @@ int main(int argc, char **argv)
 	printf("\n************\n\n");
 	sort("pb", &stack_a, &stack_b);
 	print_stacks(stack_a, stack_b);
+	printf("pb");
 
 	printf("\n************\n\n");
 	sort("pa", &stack_a, &stack_b);
 	print_stacks(stack_a, stack_b);
+	printf("pa");
 
 	printf("\n************\n\n");
 	sort("ra", &stack_a, &stack_b);
 	print_stacks(stack_a, stack_b);
+	printf("ra");
+	
+		/*push stack in stack b.*/
+		// printf("\n************\n\n");
+		// sort("pb", &stack_a, &stack_b);
+		// print_stacks(stack_a, stack_b);
+		
+		// printf("\n************\n\n");
+		// sort("pb", &stack_a, &stack_b);
+		// print_stacks(stack_a, stack_b);
 
-	// printf("\n************\n\n");
-	// sort("rb", &stack_a, &stack_b);
-	// print_stacks(stack_a, stack_b);
+	printf("\n************\n\n");
+	sort("rr", &stack_a, &stack_b);
+	print_stacks(stack_a, stack_b);
+	printf("rr");
+	
+	printf("\n************\n\n");
+	sort("sa", &stack_a, &stack_b);
+	print_stacks(stack_a, stack_b);
+	printf("sa");
+	
+	printf("\n************\n\n");
+	sort("sb", &stack_a, &stack_b);
+	print_stacks(stack_a, stack_b);
+	printf("sb");
 }
--- move1.c
+++ move1.c
@@ -6,7 +6,7 @@
 /*   By: zlaarous <zlaarous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 15:29:08 by zlaarous          #+#    #+#             */
-/*   Updated: 2023/02/26 17:01:43 by zlaarous         ###   ########.fr       */
+/*   Updated: 2023/04/16 20:59:47 by zlaarous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,45 @@ void	rotate(t_element **stack)
 	tmp->value = first;
 }
 
+void	rotate_both(t_element **stack_a, t_element **stack_b)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+}
+
+size_t	element_size(t_element *lst)
+{
+	size_t	size;
+
+	size = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		size++;
+	}
+	return (size);
+}
+
+void	swap(t_element **stack)
+{
+	int	t;
+	t_element *next_node;
+
+	if (element_size(*stack) < 2)
+		return ;
+	next_node = (*stack)->next;
+	t = (*stack)->value;
+	(*stack)->value = next_node->value;//(*stack)->next->value;
+	next_node->value = t;
+	// (*stack)->next->value = t;
+}
+
+void	swap_both(t_element **stack_a, t_element **stack_b)
+{
+	swap(stack_a);
+	swap(stack_b);
+}
+
 void	sort(char *move, t_element **stack_a, t_element **stack_b)
 {
 	// (void)stack_a;
@@ -60,8 +99,16 @@ void	sort(char *move, t_element **stack_a, t_element **stack_b)
 	{
 		rotate(stack_a);
 	}
-	else if(ft_strcmp(move, "rb"))
+	// else if(ft_strcmp(move, "rr"))
+	// {
+	// 	rotate_both(stack_a ,stack_b);
+	// }
+	else if(ft_strcmp(move, "sa"))
 	{
-		rotate(stack_b);
+		swap(stack_a);
 	}
+	// else if(ft_strcmp(move, "sb"))
+	// {
+	// 	swap(stack_b);
+	// }
 }
